b0e8346632aa797ee056c4a1dda7caec8826200d
cristianpiuca <cristianalexanderp1995@gmail.com>
2022-09-12 19:21:12 -0300

    end

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+const sumar = require("./sumar")
+
+const restar = require("./restar")
+
+const multiplicar = require("./multiplicar")
+
+const dividir = require("./dividir")
+
+
+console.log(sumar(2,5));
+console.log(restar(5,5));
+console.log(multiplicar(10,2));
+console.log(dividir(10,0));
+console.log(dividir(0,10));
